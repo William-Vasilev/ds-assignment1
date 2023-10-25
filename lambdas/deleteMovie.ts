9eb15b98fba1fab8bd764e20ecd3f989168bcf4c
williamvasilev <74902089+William-Vasilev@users.noreply.github.com>
2023-10-25 11:00:13 +0100

    Exercise: Delete movie endpoint

---
+++ lambdas/deleteMovie.ts
@@ -0,0 +1,67 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DeleteCommand, DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbDocClient = createDDbDocClient();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => { // Note change
+  try {
+    console.log("Event: ", event);
+    // const parameters = event?.queryStringParameters;
+    // const movieId = parameters ? parseInt(parameters.movieId) : undefined;
+    const parameters  = event?.pathParameters;
+    const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
+
+    if (!movieId) {
+      return {
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Invalid movie Id" }),
+      };
+    }
+
+     // Create a DeleteCommand to delete the movie based on movieId
+     await ddbDocClient.send(
+        new DeleteCommand({
+          TableName: process.env.TABLE_NAME,
+          Key: { movieId: movieId },
+        })
+      );
+      
+    // Return Response
+    return {
+        statusCode: 200,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Deleted successfully"}),
+      };
+
+    } catch (error: any) {
+      console.log(JSON.stringify(error));
+      return {
+        statusCode: 500,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ error }),
+      };
+    }
+  };
+
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -10,6 +10,7 @@ import { movies } from "../seed/movies";
 import * as apig from "aws-cdk-lib/aws-apigateway";
 
 
+
 export class RestAPIStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -83,11 +84,25 @@ export class RestAPIStack extends cdk.Stack {
             REGION: "eu-west-1",
           },
         });
+
+        const deleteMovieFn = new lambdanode.NodejsFunction(this, 'DeleteMovieFn', {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_16_X,
+          entry: `${__dirname}/../lambdas/deleteMovie.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: moviesTable.tableName,
+          },
+        });
+        
         
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
         moviesTable.grantReadWriteData(newMovieFn)
+        moviesTable.grantReadWriteData(deleteMovieFn);
+
 
 
         //Rest API
@@ -120,6 +135,11 @@ export class RestAPIStack extends cdk.Stack {
         moviesEndpoint.addMethod(
           "POST",
           new apig.LambdaIntegration(newMovieFn, { proxy: true })
+        );
+
+        movieEndpoint.addMethod(
+          "DELETE",
+          new apig.LambdaIntegration(deleteMovieFn, { proxy: true })
         );
         
         
